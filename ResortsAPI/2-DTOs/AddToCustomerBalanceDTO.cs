416835a532c89d1e891119b4110c8e6e95defe90
hillbillyjoe41 <157071173+hillbillyjoe41@users.noreply.github.com>
2025-04-13 12:59:39 -0500

    Initial outlining of endpoints and DTOs

---
+++ ResortsAPI/2-DTOs/AddToCustomerBalanceDTO.cs
@@ -0,0 +1,7 @@
+namespace ResortsAPI.DTOs;
+
+public class AddToCustomerBalanceDTO
+{
+    public string? CustomerEmail { get; set; }
+    public string? Amount { get; set; }
+}
---
+++ ResortsAPI/2-DTOs/UpdateResortPriceDTO.cs
@@ -0,0 +1,7 @@
+namespace ResortsAPI.DTOs;
+
+public class UpdateResortPriceDTO
+{
+    public string? ResortEmail { get; set; }
+    public string? Price { get; set; }
+}
