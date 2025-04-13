9ef2b964ce49971922a7298c0bd914ce4226ac5b
hillbillyjoe41 <157071173+hillbillyjoe41@users.noreply.github.com>
2025-04-13 14:36:21 -0500

    Test

--- ResortsAPI/obj/Debug/net9.0/ResortsAPI.AssemblyInfo.cs
+++ ResortsAPI/obj/Debug/net9.0/ResortsAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ResortsAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+649fac9388b3cba627dff25ac015df313294ca4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51c9210620ace5e0df2ba0791dbc1812718ba37c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ResortsAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ResortsAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
