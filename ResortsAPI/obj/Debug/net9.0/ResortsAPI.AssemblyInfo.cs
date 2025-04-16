79386a1bd560e4228b07447dbca12f4beebb4f37
hillbillyjoe41 <157071173+hillbillyjoe41@users.noreply.github.com>
2025-04-15 21:42:13 -0500

    Works with all endpoints so far

--- ResortsAPI/obj/Debug/net9.0/ResortsAPI.AssemblyInfo.cs
+++ ResortsAPI/obj/Debug/net9.0/ResortsAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ResortsAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4d59f9dd3ad0d178e11c686bd96fa5e8d17d889")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a88c8e4a5811ebc36b3a2de204dea2644f81366")]
 [assembly: System.Reflection.AssemblyProductAttribute("ResortsAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ResortsAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
